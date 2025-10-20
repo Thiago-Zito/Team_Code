89663072718ac393ae375688f7660fa899df84e6
Fernando Augusto Camussi <feaugustocamussi@gmail.com>
2025-10-20 14:13:19 -0300

    Depêndencias do projeto

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ app = FastAPI(title='MVC Produtos')
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
 app.include_router(router)
+
+# pip install -r requirements.txt -> instala todas as dependências do projeto
