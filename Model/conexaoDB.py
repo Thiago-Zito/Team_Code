f0053e847233b028f1b8ce4c7969f9e51d5b34a8
KAUA RODRIGUES PARCERAO <24271513@cfp107_2.local>
2025-12-02 16:05:52 -0300

    bla

--- Model/conexaoDB.py
+++ Model/conexaoDB.py
@@ -2,38 +2,39 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import SQLAlchemyError
 
-# Criar engine e session para SQLite
+DATABASE_URL = "postgresql+psycopg2://postgres:dev1t%4024@db.gpszbdtzifhrvejsxvsi.supabase.co:5432/postgres"
+
 def get_engine_session():
     try:
-        database = "ecommerce.db"  # arquivo do banco SQLite
+        engine = create_engine(
+        DATABASE_URL,
+        echo=True,
+        pool_pre_ping=True,
+        connect_args={
+            "sslmode": "require",
+            "hostaddr": "0.0.0.0"  # força IPv4
+        }
+    )
 
-        # Cria engine para SQLite
-        engine = create_engine(f"sqlite:///{database}", echo=True)
-
-        # Testa conexão
         conn = engine.connect()
         conn.close()
 
-        SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+        SessionLocal = sessionmaker(
+            bind=engine, autoflush=False, autocommit=False
+        )
         return engine, SessionLocal
+
     except SQLAlchemyError as e:
-        print("Falha ao conectar ao banco de dados SQLite!")
+        print("Falha ao conectar ao banco de dados PostgreSQL!")
         print(f"Erro: {e}")
         return None, None
 
-# Obter engine e SessionLocal
 engine, SessionLocal = get_engine_session()
-
-# Classe base para os models
 Base = declarative_base()
 
-# Função para dependência no FastAPI
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-# criar banco de dados 
-# Base.metadata.create_all(engine)
